a53e1f0b857733f990e996a698e1badf019632d5
Paulo Salvatore <salvatoregames@gmail.com>
2021-07-21 10:12:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Frontend - Introdução
+
+## Linguagens
+
+**Linguagem de programação:** JavaScript, TypeScript, PHP, Java, Kotlin, C#, C++, Python, ...
+
+**Linguagem interpretada:** JavaScript, Python, PHP, ...
+
+Temos um código e um software que interpreta esse código, executando-o.
+
+- **Exemplo:** No Python, temos um script, que é passado para um software chamado `python.exe`. Esse software interpreta aquele código Python e e executa.
+- **Exemplo 2:** No JavaScript, temos um script, que é passado para um software, como o `Chrome.exe` ou `node.exe`, que interpreta aquele código JavaScript e executa.
+
+**Linguagem compilada:** TypeScript, Java, Kotlin, C#, C++, Python, ...
+
+Na linguagem compilada, pegamos um script naquela linguagem e transformar em um arquivo binário. Essa transformação é feita por um software preparado para isso e esse arquivo binário é executado diretamente pelo sistema operacional.
+
+- **Exemplo:** Podemos pegar um código C#, transformar ele em um arquivo binário com a extensão `.exe`, que o Windows irá executá-lo diretamente. Ou podemos pegar um código Java ou Kotlin e transformar para um arquivo `.apk`, que o Android irá executá-lo diretamente.
+- **Exemplo 2:** Pegamos um código escrito em TypeScript e transpilamos ele para JavaScript, que será interpretado direto pelo software em questão (seja o `Chrome` ou o `NodeJS`).
+
+## Frameworks e Bibliotecas
+
+- **Biblioteca:** é um pedaço de código que resolve um problema específico.
+  - **Exemplo:** `npm is-odd` resolve apenas o problema de determinar se um número é par ou ímpar.
+- **Framework:** é um conjunto de bibliotecas ou vários códigos que resolvem diversos problemas relacionados a um problema maior. Em alguns casos, o framework também pode determinar assuntos mais avançados, como arquitetura de projetos, estrutura de pastas, etc.
+  - **Exemplo:** `npm express` é um micro-framework, que resolve o problema de criar um servidor HTTP, ou seja, um servidor capaz de conversar utilizando o protocolo principal da web.
+  - **Exemplo 2:** `NestJS` é um framework que resolve o problema de criar um servidor HTTP, utilizando por baixo dos panos o `npm express`, mas criando uma arquitetura por cima, além de unificar várias outras bibliotecas que o `express` não traz.
+  - **Exemplo 3:** `npm ReactJS` é um framework que resolve o problema que criação de páginas com HTML, CSS e JavaScript. Ele não força a estrutura de pastas, mas ele oferece maneiras alternativas de construir uma página da web.
+    - HTML: linguagem de marcação - define a estrutura da página
+    - HTML -> JSX: JSX é semelhante ao que escrevemos em HTML, mas é uma adaptação para construir o HTML usando o JavaScript.
+    - CSS: linguagem de estilização - define o estilo/estilização da página (cores, tamanhos, etc)
+    - JavaScript: linguagem de programação - define o comportamento da página.
+
+
+
---
+++ src/index.js
@@ -0,0 +1,135 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./index.css";
+
+/*
+CRUD (Create, Read (Single & All), Update, Delete)
+
+Create -> Formulário de criação
+Read All -> Listagem de itens cadastrados
+Read Single -> Visualização de um item específico
+Update -> Formulário de edição
+Delete -> Remover um item específico
+*/
+
+// Read All
+// Precisa dos dados que serão exibidos
+// A estrutura desses dados precisa possuir 'Nome' e 'URL da Imagem'
+
+// Decidam qual será o tema da sua aplicação
+// O meu será de Raças de Cachorro
+
+const lista = [
+    {
+        id: 1,
+        nome: "Pinscher",
+        imagemUrl: "https://pbs.twimg.com/media/Da3FmbSWkAEuwUJ.jpg",
+    },
+    {
+        id: 2,
+        nome: "Golden Retriever",
+        imagemUrl:
+            "https://f.i.uol.com.br/fotografia/2017/10/27/150912457859f369e28eaa5_1509124578_3x2_md.jpg",
+    },
+    {
+        id: 3,
+        nome: "Labrador",
+        imagemUrl:
+            "https://www.clubeparacachorros.com.br/wp-content/uploads/2014/07/labrador-amarelo.jpg",
+    },
+    {
+        id: 4,
+        nome: "Yorkshire",
+        imagemUrl:
+            "https://www.petlove.com.br/images/breeds/192471/profile/original/yorkshire-p.jpg?1532539683",
+    },
+    {
+        id: 5,
+        nome: "Husky Siberiano",
+        imagemUrl:
+            "https://www.petlove.com.br/images/breeds/193082/profile/original/husky_siberian-p.jpg?1532539123",
+    },
+];
+
+function Item(props) {
+    const indice = props.indice;
+
+    const item = lista[indice];
+
+    return (
+        <a href={"/visualizar/" + indice}>
+            <div className="item">
+                <h1 className="item__title">{item.nome}</h1>
+                <img src={item.imagemUrl} alt={item.nome} width="200" />
+            </div>
+        </a>
+    );
+}
+
+function Lista() {
+    return (
+        <div className="lista">
+            {lista.map((item, index) => (
+                <Item indice={index} key={index} />
+            ))}
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <header className="header">
+            <a href="/">
+                <img
+                    src="https://www.oceanbrasil.com/img/general/logoOceanI.png"
+                    alt="Samsung Ocean"
+                    width="300"
+                />
+            </a>
+        </header>
+    );
+}
+
+function Footer() {
+    return <footer className="footer">Todos os direitos reservados.</footer>;
+}
+
+function ListarItens() {
+    return (
+        <div>
+            <Lista />
+        </div>
+    );
+}
+
+function VisualizarItem(props) {
+    return (
+        <div>
+            <Item indice={props.match.params.id} />
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div className="app">
+            <Header />
+            <Switch>
+                <Route path="/" exact={true} component={ListarItens} />
+
+                <Route path="/visualizar/:id" component={VisualizarItem} />
+            </Switch>
+            <Footer />
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
